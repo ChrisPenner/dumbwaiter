728238515a47413c98f3b9e0fe72ffa659627f07
Chris Penner <christopher.penner@gmail.com>
2017-12-10 12:57:09 -0600

    File watcher WIP

--- src/Dumbwaiter.hs
+++ src/Dumbwaiter.hs
@@ -20,26 +20,47 @@ import qualified Data.Text.IO as TIO
 import System.Exit (exitFailure)
 import Control.Monad.IO.Class
 import Data.Monoid
+import Control.Monad
+import Control.Concurrent
+import System.FSNotify
 
 
 data Config = Config
   { routes :: [RouteConfig]
   } deriving (Show, Generic, A.FromJSON, A.ToJSON)
 
-readConfig :: FilePath -> IO (Either String Config)
-readConfig filepath = first show <$> Y.decodeFileEither filepath
+readConfigFile :: FilePath -> IO (Either String Config)
+readConfigFile filepath = first show <$> Y.decodeFileEither filepath
 
 type Port = Int
 
 run :: [Matcher] -> [Responder] -> FilePath -> Port -> IO ()
-run matchers responders configFile port = do
-  config <- readConfig configFile
-  routes' <- case config of
-           Left err -> print err >> exitFailure
-           Right Config{routes} -> return routes
+run matchers responders configFilePath port = do
   putStrLn $ "Serving on port " <> show port
+  filewatcher configFilePath (runServer matchers responders port)
+
+runServer :: [Matcher] -> [Responder] -> Int -> [RouteConfig] -> IO ()
+runServer matchers responders port routes' =
   F.run port $ do
     path <- F.getPath
     method <- F.getMethod
     liftIO . TIO.putStrLn $ method <> " " <> path
     traverse_ (\routeConf -> buildHandler routeConf matchers responders) routes'
+
+readRoutes :: String -> IO [RouteConfig]
+readRoutes configFilePath = do
+  config <- readConfigFile configFilePath
+  case config of
+    Left err -> print err >> exitFailure
+    Right Config{routes} -> return routes
+
+
+filewatcher :: String -> ([RouteConfig] -> IO ()) -> IO ()
+filewatcher configFilePath serverRunner = withManager $ \m -> do
+  _ <- watchDir m "." matchesConfFile $ \evt -> do
+    putStrLn $ eventPath evt
+    routes' <- readRoutes configFilePath
+    serverRunner routes'
+  forever $ threadDelay 1000000 -- block forever
+    where
+      matchesConfFile = const True -- (== configFilePath) . eventPath
